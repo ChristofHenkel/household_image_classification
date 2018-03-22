7ed054a5577f59f74aad0e58a5a33a98e7fc0715
christof <christof.henkel@campus.lmu.de>
2018-03-22 07:27:38 +0100

    initial commit

---
+++ global_variables.py
@@ -0,0 +1,3 @@
+TRAIN_DATA_FN = 'assets/train.json'
+TEST_DATA_FN = 'assets/test.json'
+VALID_DATA_FN = 'assets/validation.json'
---
+++ utils.py
@@ -0,0 +1,30 @@
+import json
+import pandas as pd
+from global_variables import TRAIN_DATA_FN, TEST_DATA_FN, VALID_DATA_FN
+
+#load data
+def save_traing_df():
+    data = json.load(open(TRAIN_DATA_FN))
+    #Saved On Data Frame
+    data_url = pd.DataFrame.from_dict(data['images'])
+    labels = pd.DataFrame.from_dict(data['annotations'])
+    train_data = data_url.merge(labels, how='inner', on=['image_id'])
+    train_data['url'] = train_data['url'].str.get(0)
+    del data, data_url, labels
+    train_data.to_csv('assets/train_data.csv')
+
+def save_valid_df():
+    data = json.load(open(VALID_DATA_FN))
+    #Saved On Data Frame
+    data_url = pd.DataFrame.from_dict(data['images'])
+    labels = pd.DataFrame.from_dict(data['annotations'])
+    valid_data = data_url.merge(labels, how='inner', on=['image_id'])
+    valid_data['url'] = valid_data['url'].str.get(0)
+    del data, data_url, labels
+    valid_data.to_csv('assets/valid_data.csv')
+
+def save_test_df():
+    data = json.load(open(TEST_DATA_FN))
+    #Saved On Data Frame
+    data_url = pd.DataFrame.from_dict(data['images'])
+    data_url.to_csv('assets/test_data.csv', index=False)
