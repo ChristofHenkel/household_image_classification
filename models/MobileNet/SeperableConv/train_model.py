8667a4d538c0d7b0de29e59b735f2b0b0758f88b
christof <christof.henkel@campus.lmu.de>
2018-04-02 19:04:10 +0200

    changes

--- models/MobileNet/SeperableConv/train_model.py
+++ models/MobileNet/SeperableConv/train_model.py
@@ -2,8 +2,7 @@ from keras.applications.mobilenet import MobileNet
 from keras.preprocessing.image import ImageDataGenerator
 from keras.layers import Dense, Concatenate,Bidirectional, Flatten, Conv2D, MaxPooling2D,GlobalMaxPooling2D, ConvLSTM2D, TimeDistributed , CuDNNLSTM,GlobalAveragePooling2D
 from keras.models import Model, load_model
-from keras.optimizers import Adam, SGD
-from keras.losses import categorical_crossentropy, categorical_hinge
+from keras.optimizers import Adam
 from keras.callbacks import EarlyStopping,ModelCheckpoint
 from keras.metrics import top_k_categorical_accuracy
 from utilities import top1_loss
@@ -13,8 +12,7 @@ train_data_gen = ImageDataGenerator(rescale=1./255,
                                     rotation_range=20,
                                     width_shift_range=0.2,
                                     height_shift_range=0.2,
-                                    zoom_range=0.2,
-                                    shear_range=0.2
+                                    zoom_range=0.2
                                     )
 train_generator = train_data_gen.flow_from_directory(directory='assets/train/',
                              target_size=(224,224),
@@ -27,9 +25,8 @@ valid_generator = valid_data_gen.flow_from_directory(directory='assets/valid/',
                             batch_size=32,
                              class_mode='categorical')
 
-
 base_model = MobileNet(weights='imagenet', include_top=False,input_shape=(224,224,3))
-top_model = load_model('models/MobileNet/Dense/top_model.hdf5', custom_objects={'top1_loss':top1_loss})
+top_model = load_model('models/MobileNet/SeperableConv/top_model.hdf5', custom_objects={'top1_loss':top1_loss})
 x = base_model.output
 predictions = top_model(x)
 
@@ -39,9 +36,10 @@ model = Model(inputs=base_model.input, outputs=predictions)
 
 
 model.summary()
-model.compile(optimizer=SGD(momentum=0.9,lr = 0.0001,decay=0.001), loss=categorical_crossentropy,metrics=[top1_loss])
-file_path = 'models/MobileNet/Dense/mobilenet_dense.hdf5'
-check_point = ModelCheckpoint(file_path, monitor="val_top1_loss", mode="min", save_best_only=True, verbose=1)
+model.compile(optimizer=Adam(lr = 0.00005), loss='categorical_crossentropy',metrics=[top1_loss])
+file_path = 'models/MobileNet/SeperableConv/mobilenet_sepconv.hdf5'
+# train the model on the new data for a few epochs
+check_point = ModelCheckpoint(file_path, monitor="val_loss", mode="min", save_best_only=True, verbose=1)
 early_stop = EarlyStopping(patience=3)
 history = model.fit_generator(train_generator,
                               steps_per_epoch=5400,
---
+++ models/Xception/predict_base_model.py
@@ -0,0 +1,22 @@
+from tqdm import tqdm
+from keras.preprocessing.image import ImageDataGenerator
+import os
+from keras.applications.xception import Xception
+import pickle
+import gzip
+
+maxlen = 192171
+img_nums = {i:len(os.listdir('assets/train_224/'+ str(i) + '/')) for i in range(1,129)}
+train_data_gen = ImageDataGenerator(rescale=1. / 255)
+train_generator = train_data_gen.flow_from_directory(directory='assets/train_224/',
+                                                     target_size=(224, 224),
+                                                     batch_size=32,
+                                                    class_mode='categorical', shuffle=False)
+
+base_model = Xception(weights='imagenet', include_top=False,input_shape=(224,224,3))
+
+for b in tqdm(range(maxlen//32)):
+    x = train_generator.next()
+    z = base_model.predict_on_batch(x[0])
+    with gzip.open('assets/bn_xception_train_224/' + str(b) + '.p','wb',compresslevel=6) as f:
+        pickle.dump((z,x[1]),f)
